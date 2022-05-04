d436df5c723e8f027f764c165da4ec44ab3592cb
poko <minshanzen@gmail.com>
2022-05-04 21:40:31 +0800

    update script version.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "Ver.2022/05/04-002"
+const version = "Ver.2022/05/04-003"
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const country = urlParams.get('country')
